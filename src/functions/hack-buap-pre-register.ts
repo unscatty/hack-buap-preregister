86cf1f924278a7efb28d92fa638f9e41ba4aac63
Carlos Aguilera <ckarlos_95@hotmail.es>
2024-02-03 20:50:46 -0600

    [WIP] feat(user): add initial registration

--- src/functions/hack-buap-pre-register.ts
+++ src/functions/hack-buap-pre-register.ts
@@ -4,20 +4,49 @@ import {
   HttpResponseInit,
   InvocationContext,
 } from '@azure/functions'
+import { createDrizzle } from '../create-drizzle'
+import { UserInsert, users } from '../schema'
+import { eq } from 'drizzle-orm'
 
-export async function hackBuapPreRegister(
+const createDB = createDrizzle(process.env['DatabaseConnectionString']!)
+
+// TODO: validate incoming data using TypeBox/Zod
+// TODO: success/error message builder
+export async function handleBuapPreRegister(
   request: HttpRequest,
   context: InvocationContext
 ): Promise<HttpResponseInit> {
-  context.log(`Http function processed request for url "${request.url}"`)
+  const db = await createDB
 
-  const name = request.query.get('name') || (await request.text()) || 'world'
+  const userData = (await request.json()) as unknown as UserInsert
 
-  return { body: `Hello, ${name}!` }
+  console.log(userData)
+
+  const existingUser = await db.query.users.findFirst({
+    where: eq(users.email, userData.email),
+  })
+
+  if (existingUser) {
+    return {
+      status: 409,
+      jsonBody: { success: false, error: 'User already exists' },
+    }
+  }
+
+  try {
+    await db.insert(users).values(userData)
+  } catch {
+    return {
+      status: 500,
+      jsonBody: { success: false, error: 'Database error' },
+    }
+  }
+
+  return { jsonBody: { success: true, message: 'User registered' } }
 }
 
 app.http('hack-buap-pre-register', {
-  methods: ['GET', 'POST'],
+  methods: ['POST'],
   authLevel: 'anonymous',
-  handler: hackBuapPreRegister,
+  handler: handleBuapPreRegister,
 })
