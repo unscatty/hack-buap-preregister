7d770244274ebeac01fd6072f43e8517209d026d
Carlos Aguilera <ckarlos_95@hotmail.es>
2024-02-06 00:20:14 -0600

    chore(playground): update deps

--- src/email/playground/vite.config.ts
+++ src/email/playground/vite.config.ts
@@ -1,11 +1,13 @@
 import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import FullReload from 'vite-plugin-full-reload'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    FullReload('../templates/*.vue'),
   ],
   resolve: {
     alias: {
